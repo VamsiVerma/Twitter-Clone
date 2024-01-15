d6f07bc7fe8b0a74e2496d0002a217ddf432d80b
Vamsi Verma K <vamsiverma021@gmail.com>
2024-01-15 15:25:36 +0530

    Skeleton of whats happening and who to follow

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -13,16 +13,16 @@ function Footer() {
   return (
     <div className="d-flex flex-column">
       <div id="searchBar" className="search-bar ">
-        <div className="search-bar-container d-flex flex-column">
+        <div className="search-bar-container d-flex flex-column mb-16">
           <SearchBar setResults={setResults} />
           <SearchResultsList results={results} />
         </div>
       </div>
-      <aside
+      <div
         id="premium-section"
-        className="bg-light d-flex overflow-hidden premium-notification footer-border"
+        className="bg-light d-flex overflow-hidden premium-notification footer-border w-100 br-16 mb-16"
       >
-        <div className="sp-container d-flex flex-column">
+        <div className="sp-container d-flex flex-column p-16">
           <div className="spheading">
             <span className="spData">Subscribe to Premium</span>
           </div>
@@ -38,7 +38,21 @@ function Footer() {
             </button>
           </div>
         </div>
-      </aside>
+      </div>
+      <div className="bg-light d-flex overflow-hidden happening footer-border br-16 mb-16">
+        <div className="p-16">
+          <div className="spheading">
+            <span>What's happening</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-light d-flex overflow-hidden follow-section footer-border br-16 mb-16">
+        <div className="p-16">
+          <div className="spheading">
+            <span>Who to follow</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
